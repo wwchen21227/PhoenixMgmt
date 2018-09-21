d383b8b8afeb0c3b0ec131e283be6bb0bf406c25
Chee Kit <kookit@gmail.com>
2018-09-21 16:20:16 +0800

    Schedule Controller

---
+++ src/java/sg/edu/nus/iss/phoenix/schedule/controller/DeleteAnnualScheduleCmd.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.phoenix.schedule.controller;
+
+import at.nocturne.api.Action;
+import at.nocturne.api.Perform;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import sg.edu.nus.iss.phoenix.schedule.delegate.ScheduleDelegate;
+
+/**
+ *
+ * @author kooc
+ */
+@Action("deleteas")
+public class DeleteAnnualScheduleCmd implements Perform {
+        @Override
+    public String perform(String path, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        ScheduleDelegate del = new ScheduleDelegate();
+        String year = req.getParameter("year");
+        del.processDeleteAS(Integer.parseInt(year));
+        return "/pages/crudas.jsp";
+    }  
+}
