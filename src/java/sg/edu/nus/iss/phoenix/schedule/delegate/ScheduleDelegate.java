784dd42f559f126433ceadbf0bca54ce918f719a
Chee Kit <kookit@gmail.com>
2018-09-21 16:19:27 +0800

    ScheduleDelegate

---
+++ src/java/sg/edu/nus/iss/phoenix/schedule/delegate/ScheduleDelegate.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.phoenix.schedule.delegate;
+
+import java.util.ArrayList;
+import java.sql.Date;
+
+import sg.edu.nus.iss.phoenix.schedule.entity.AnnualSchedule;
+import sg.edu.nus.iss.phoenix.schedule.entity.WeeklySchedule;
+import sg.edu.nus.iss.phoenix.schedule.service.ScheduleService;
+
+/**
+ *
+ * @author kooc
+ */
+
+public class ScheduleDelegate {
+    
+    	public ArrayList<AnnualSchedule> findAllAS() {
+		ScheduleService service = new ScheduleService();
+		return service.findAllAS();
+		
+	}
+	
+	public void processCreateAS(AnnualSchedule as) {
+		ScheduleService service = new ScheduleService();
+		service.processCreateAS(as);
+		
+	}
+	public void processModifyAS(AnnualSchedule as) {
+		ScheduleService service = new ScheduleService();
+		service.processModifyAS(as);
+		
+	}
+
+	public void processDeleteAS(Integer year) {
+		ScheduleService service = new ScheduleService();
+		service.processDeleteAS(year);
+        }
+        
+        //=
+        public ArrayList<WeeklySchedule> findAllWS() {
+		ScheduleService service = new ScheduleService();
+		return service.findAllWS();
+		
+	}
+	
+	public void processCreateWS(WeeklySchedule ws) {
+		ScheduleService service = new ScheduleService();
+		service.processCreateWS(ws);
+		
+	}
+	public void processModifyWS(WeeklySchedule ws) {
+		ScheduleService service = new ScheduleService();
+		service.processModifyWS(ws);
+		
+	}
+
+	public void processDeleteWS(Date startDate) {
+		ScheduleService service = new ScheduleService();
+		service.processDeleteWS(startDate);
+        }
+                
+        
+}
