1ee39b05c95188a9405bf559245f134e91692568
sourcepirate <sathyanarrayanan@yandex.com>
2018-09-22 19:58:02 +0800

    feat(), fix()  added dao and fixed the mysql access issue when container is created'

---
+++ src/java/sg/edu/nus/iss/phoenix/authenticate/dao/PresentorDao.java
@@ -0,0 +1,46 @@
+package sg.edu.nus.iss.phoenix.authenticate.dao;
+
+import java.util.List;
+import sg.edu.nus.iss.phoenix.authenticate.entity.User;
+import sg.edu.nus.iss.phoenix.core.exceptions.NotFoundException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sourcepirate
+ */
+public interface PresentorDao {
+     /**
+     * method is used by any object to create a new producer value
+     * instance.
+     * @return
+     */
+    public abstract User CreateValueObject();
+    
+    /**
+     * method is used by any object to list all producer instances
+     * @return
+     */
+    public abstract List<User> getAll();
+
+    /**
+     *
+     * @param id
+     * @return
+     * @throws NotFoundException
+     */
+    public abstract User selectPresentor(String id) throws NotFoundException;
+    
+    /**
+     *
+     * @param prefix
+     * @return
+     * @throws NotFoundException
+     */
+    public abstract List<User> searchMatching(String prefix) throws NotFoundException;   
+}
---
+++ src/java/sg/edu/nus/iss/phoenix/authenticate/dao/ProducerDao.java
@@ -0,0 +1,43 @@
+package sg.edu.nus.iss.phoenix.authenticate.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import sg.edu.nus.iss.phoenix.authenticate.entity.User;
+import sg.edu.nus.iss.phoenix.core.exceptions.NotFoundException;
+
+/**
+ *
+ * @author sourcepirate
+ */
+public interface ProducerDao {
+    
+    /**
+     * method is used by any object to create a new producer value
+     * instance.
+     * @return
+     */
+    public abstract User CreateValueObject();
+    
+    /**
+     * method is used by any object to list all producer instances
+     * @return
+     */
+    public abstract List<User> getAll();
+
+    /**
+     *
+     * @param id
+     * @return
+     * @throws NotFoundException
+     */
+    public abstract User selectProducer(String id) throws NotFoundException;
+    
+    /**
+     *
+     * @param prefix
+     * @return
+     * @throws NotFoundException
+     */
+    public abstract List<User> searchMatching(String prefix) throws NotFoundException;
+}
